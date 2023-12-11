0059b9c20f36712ec536274f19d9989db6a40562
Bart <bartek@redly.pl>
2023-12-11 07:06:33 +0100

    added .npmrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,41 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Maroon Studio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Maroon Studio`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+    plugins: [
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-mdx",
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                trackingIds: ["G-CEQLH3SY3D"],
+                pluginConfig: {
+                    head: true,
+                },
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+    ],
+}
