a82c565cdbc413ab5f1653ccef286dd4b46e7146
Bart <bart@maroon-studio.com>
2024-05-31 07:19:41 +0200

    fix: fix 404 page outdated layout import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,11 @@
 import * as React from "react"
-import { GlobalStyles } from "../styles/globalStyles"
-import { Layout } from "../components/layout/index.style"
-import Navbar from "../components/navbar"
+import Layout from "../components/layout"
 import PageNotFoundTemplate from "../templates/index/pageNotFound"
 
 const NotFoundPage = () => {
     return (
         <>
             <Layout>
-                <GlobalStyles />
-                <Navbar />
                 <PageNotFoundTemplate />
             </Layout>
         </>
