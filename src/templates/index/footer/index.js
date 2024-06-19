df666ba379a3b5da40cbfbbd6d9b690935fb88f9
Krystian Mariampolski <mariampolskiprywatnie@gmail.com>
2024-06-19 22:42:27 +0200

    fix: replace footer social media icons using graphql

--- src/templates/index/footer/index.js
+++ src/templates/index/footer/index.js
@@ -1,12 +1,8 @@
 import React from "react"
 
 import LogoFooter from "../../../components/footerLogo/index.js"
-import InstagramIcon from "../../../images/maroonStudio-instagramIcon.png"
-import WhatsappIcon from "../../../images/maroonStudio-whatsappIcon.png"
-import XIcon from "../../../images/maroonStudio-xIcon.png"
-import FacebookIcon from "../../../images/maroonStudio-facebookIcon.png"
-import BehanceIcon from "../../../images/maroonStudio-behanceIcon.png"
-import LinkedinIcon from "../../../images/maroonStudio-linkedinIcon.png"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 import {
     FooterIcon,
@@ -24,48 +20,6 @@ import {
 import { FooterIconsWrapper } from "./index.style"
 import { FooterWrapper } from "./index.style"
 import { FooterFirstLine } from "./index.style"
-
-const footerIconsData = [
-    {
-        id: 1,
-        name: "InstagramIcon",
-        alt: "Instagram",
-    },
-    {
-        id: 2,
-        name: "WhatsappIcon",
-        alt: "Whatsapp",
-    },
-    {
-        id: 3,
-        name: "XIcon",
-        alt: "X",
-    },
-    {
-        id: 4,
-        name: "FacebookIcon",
-        alt: "Facebook",
-    },
-    {
-        id: 5,
-        name: "BehanceIcon",
-        alt: "Behance",
-    },
-    {
-        id: 6,
-        name: "LinkedinIcon",
-        alt: "Linkedin",
-    },
-]
-
-const iconMap = {
-    InstagramIcon,
-    WhatsappIcon,
-    XIcon,
-    FacebookIcon,
-    BehanceIcon,
-    LinkedinIcon,
-}
 
 const footerLinksData = [
     {
@@ -94,23 +48,124 @@ const footerLinksData = [
         link: "#",
     },
 ]
+
 const Footer = () => {
+    const FooterIconsData = useStaticQuery(graphql`
+        query {
+            InstagramIcon: file(
+                relativePath: { eq: "maroonStudio-instagramIcon.png" }
+            ) {
+                childImageSharp {
+                    fluid(maxHeight: 25, maxWidth: 25) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            WhatsappIcon: file(
+                relativePath: { eq: "maroonStudio-whatsappIcon.png" }
+            ) {
+                childImageSharp {
+                    fluid(maxHeight: 25, maxWidth: 25) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            XIcon: file(relativePath: { eq: "maroonStudio-xIcon.png" }) {
+                childImageSharp {
+                    fluid(maxHeight: 25, maxWidth: 25) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            FacebookIcon: file(
+                relativePath: { eq: "maroonStudio-facebookIcon.png" }
+            ) {
+                childImageSharp {
+                    fluid(maxHeight: 25, maxWidth: 25) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            BehanceIcon: file(
+                relativePath: { eq: "maroonStudio-behanceIcon.png" }
+            ) {
+                childImageSharp {
+                    fluid(maxHeight: 25, maxWidth: 25) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            LinkedinIcon: file(
+                relativePath: { eq: "maroonStudio-linkedinIcon.png" }
+            ) {
+                childImageSharp {
+                    fluid(maxHeight: 25, maxWidth: 25) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `)
+
+    const footerIconsData = [
+        {
+            id: 1,
+            name: "InstagramIcon",
+            alt: "Instagram",
+            data: FooterIconsData.InstagramIcon,
+        },
+        {
+            id: 2,
+            name: "WhatsappIcon",
+            alt: "Whatsapp",
+            data: FooterIconsData.WhatsappIcon,
+        },
+        {
+            id: 3,
+            name: "XIcon",
+            alt: "X",
+            data: FooterIconsData.XIcon,
+        },
+        {
+            id: 4,
+            name: "FacebookIcon",
+            alt: "Facebook",
+            data: FooterIconsData.FacebookIcon,
+        },
+        {
+            id: 5,
+            name: "BehanceIcon",
+            alt: "Behance",
+            data: FooterIconsData.BehanceIcon,
+        },
+        {
+            id: 6,
+            name: "LinkedinIcon",
+            alt: "Linkedin",
+            data: FooterIconsData.LinkedinIcon,
+        },
+    ]
+
     return (
         <>
             <FooterWrapper>
                 <FooterFirstLine>
                     <LogoFooter />
                     <SocialIconsWrapper>
-                        {footerIconsData.map(({ id, name, alt }) => {
-                            const Icon = iconMap[name]
-                            return (
-                                <FooterIconsWrapper key={id}>
-                                    <FooterIcon>
-                                        <img src={Icon} alt={alt} />
-                                    </FooterIcon>
-                                </FooterIconsWrapper>
-                            )
-                        })}
+                        {footerIconsData.map(({ id, alt, data }) => (
+                            <FooterIconsWrapper key={id}>
+                                <FooterIcon>
+                                    <Img
+                                        fluid={data.childImageSharp.fluid}
+                                        alt={alt}
+                                        style={{
+                                            width: "100%",
+                                            height: "100%",
+                                        }}
+                                    />
+                                </FooterIcon>
+                            </FooterIconsWrapper>
+                        ))}
                     </SocialIconsWrapper>
                 </FooterFirstLine>
                 <FooterSecondLine>
