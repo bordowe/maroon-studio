2212fa941b7b5d1c331be977c1168c918560ed1c
Bart <bart@maroon-studio.com>
2024-07-07 15:26:46 +0200

    react-helmet-initialization

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,19 +1,44 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { LayoutWrapper } from "./index.style"
 import { GlobalStyles } from "../../styles/globalStyles"
 import Navbar from "../navbar/index"
 import Footer from "../../templates/index/footer/index"
 import MobileNavbarDropdown from "../../components/mobileNavbarDropdown"
+import { Helmet } from "react-helmet"
 
 const Layout = (props) => {
     const [currentSection, setCurrentSection] = useState("heroBody")
+    const [locationGet, setLocationGet] = useState("")
+    const [originlocationGet, setOriginLocationGet] = useState("")
 
     const handleSwitchSection = (section) => {
         setCurrentSection(section)
     }
 
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const currentPathname = window.location.pathname
+            const replaceCurrentPathname = currentPathname.replace(/^\//, "")
+            const spacedCapitalizationReplaceCurrentPathname =
+                replaceCurrentPathname
+                    .split("-")
+                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                    .join(" ")
+            setLocationGet(spacedCapitalizationReplaceCurrentPathname)
+
+            setOriginLocationGet(window.location.href)
+        }
+    }, [])
+
     return (
         <LayoutWrapper>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>
+                    Maroon Studio {locationGet === "" ? "" : `- ${locationGet}`}
+                </title>
+                <link rel="cannonical" href={originlocationGet} />
+            </Helmet>
             <GlobalStyles />
             <Navbar onToggleSection={handleSwitchSection} />
             {currentSection === "heroBody" && props.children}
