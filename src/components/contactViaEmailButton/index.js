4aa889b44490f2aee926e6f9fae8819d22e5c6e5
Bart <bart@maroon-studio.com>
2024-07-18 16:21:07 +0200

    feat: add integration for ga4

--- src/components/contactViaEmailButton/index.js
+++ src/components/contactViaEmailButton/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import ReactGA from "react-ga4"
 
 import {
     InterestSectionLeftColumnEmailButton,
@@ -6,11 +7,20 @@ import {
     InterestSectionLeftColumnEmailButtonSubtitle,
 } from "./index.style"
 
+const handleViaEmailClick = () => {
+    ReactGA.event({
+        category: "Email",
+        action: "Click",
+        label: "Via Email Button",
+    })
+}
+
 const ContactViaEmailButton = () => {
     return (
         <a
             href="mailto:contact@maroon-studio.com"
             style={{ textDecoration: "none", color: "#73013C" }}
+            onClick={handleViaEmailClick}
         >
             <InterestSectionLeftColumnEmailButton>
                 <InterestSectionLeftColumnEmailButtonTitle>
